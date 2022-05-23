f9c4ad704c8099fabf45d371c1de93ead4851151
Miquel Àngel Ramis <32435309+MiquelQuely@users.noreply.github.com>
2022-05-23 21:24:09 +0000

    added new dependencies in mix

--- sandbox/mix.exs
+++ sandbox/mix.exs
@@ -48,7 +48,10 @@ defmodule Sandbox.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:arrays, "~> 2.0"},
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:mimic, "~> 1.7", only: :test}
     ]
   end
 
